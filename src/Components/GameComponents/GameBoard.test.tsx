404af992651d7fc63943faf0585b8b592a9c573e
Will Wedmedyk <willwedmedyk@gmail.com>
2013-08-15 19:32:10 -0400

    ww/update

--- src/Components/GameComponents/GameBoard.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-
-describe('GameBoard', () => {
-  it('renders the scoreboard header', () => {
-    // const { getByText } = render(<Scoreboard scores={scores} />);
-    expect(1).toEqual(1);
-  });
-});
