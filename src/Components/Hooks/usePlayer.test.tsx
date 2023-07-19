7bd42e7b8b753cda708bfc8d4a54df1f1efdf439
Will Wedmedyk <willwedmedyk@gmail.com>
2023-07-19 14:55:11 -0400

    ww/useplayer hook + tests

---
+++ src/Components/Hooks/usePlayer.test.tsx
@@ -0,0 +1,165 @@
+import { renderHook, act } from '@testing-library/react';
+import usePlayer from './usePlayer';
+import { TableCard, Player } from '../../types';
+
+describe('usePlayer', () => {
+  const initialPlayers: Player[] = [
+    {
+      id: 'player0',
+      hand: [],
+      score: 0,
+      wonCardsTotal: 0,
+      isPicker: false,
+      wonCards: [],
+      cardToPlay: {} as TableCard
+    },
+    {
+      id: 'player1',
+      hand: [],
+      score: 0,
+      wonCardsTotal: 0,
+      isPicker: false,
+      wonCards: [],
+      cardToPlay: {} as TableCard
+    },
+    {
+      id: 'player2',
+      hand: [],
+      score: 0,
+      wonCardsTotal: 0,
+      isPicker: false,
+      wonCards: [],
+      cardToPlay: {} as TableCard
+    }
+  ];
+
+  it('should initialize with the provided players', () => {
+    const { result } = renderHook(() => usePlayer(initialPlayers));
+    const { players } = result.current;
+    expect(players).toEqual(initialPlayers);
+  });
+
+  it('should set the hand for a player', () => {
+    const { result } = renderHook(() => usePlayer(initialPlayers));
+    const { setHand } = result.current;
+    const playerId = 'player0';
+    const hand = ['AH', 'KH', 'QH'];
+
+    act(() => {
+      setHand(playerId, hand);
+    });
+
+    const { players } = result.current;
+    const updatedPlayer = players.find(player => player.id === playerId);
+    expect(updatedPlayer?.hand).toEqual(hand);
+  });
+
+  it('should make player picker, and push blind cards to won pile', () => {
+    const { result } = renderHook(() => usePlayer(initialPlayers));
+    const { makePicker } = result.current;
+    const playerId = 'player0';
+    const cards = ['AH', 'KH'];
+
+    act(() => {
+      makePicker(playerId, cards);
+    });
+
+    const { players } = result.current;
+    const updatedPlayer = players.find(player => player.id === playerId);
+    expect(updatedPlayer?.wonCards).toEqual(cards);
+    expect(updatedPlayer?.isPicker).toEqual(true);
+  });
+
+  it("should add a card to a player's won cards pile", () => {
+    const { result } = renderHook(() => usePlayer(initialPlayers));
+    const { addWonCard } = result.current;
+    const playerId = 'player1';
+    const cards = ['AH', 'KH'];
+
+    act(() => {
+      addWonCard(playerId, cards);
+    });
+
+    const { players } = result.current;
+    const updatedPlayer = players.find(player => player.id === playerId);
+    expect(updatedPlayer?.wonCards).toEqual(cards);
+  });
+
+  it('should play a card for a player', () => {
+    const { result } = renderHook(() => usePlayer(initialPlayers));
+    const { playCard, setHand } = result.current;
+    const hand = ['AH', 'KH', 'JC'];
+    const playerId = 'player2';
+    const card = 'JC';
+
+    act(() => {
+      setHand(playerId, hand);
+      playCard(playerId, card);
+    });
+
+    const { players } = result.current;
+    const updatedPlayer = players.find(player => player.id === playerId);
+    expect(updatedPlayer?.hand).not.toContain(card);
+    expect(updatedPlayer?.cardToPlay).toEqual({ player: playerId, card });
+  });
+
+  it('should calculate all won cards totals', () => {
+    const { result } = renderHook(() => usePlayer(initialPlayers));
+    const { addWonCard, totalWonCards } = result.current;
+
+    const player0 = 'player0';
+    const cards0 = ['AC', 'KS', 'QS'];
+
+    const player1 = 'player1';
+    const cards1 = ['AH', 'KH', 'SH'];
+
+    const player2 = 'player2';
+    const cards2 = ['JD', 'SS', 'ES'];
+
+    act(() => {
+      addWonCard(player0, cards0);
+      addWonCard(player1, cards1);
+      addWonCard(player2, cards2);
+      totalWonCards();
+    });
+
+    const { players } = result.current;
+    expect(players[0].wonCardsTotal).toEqual(18);
+    expect(players[1].wonCardsTotal).toEqual(15);
+    expect(players[2].wonCardsTotal).toEqual(2);
+  });
+
+  it('should reset for the next turn', () => {
+    const { result } = renderHook(() => usePlayer(initialPlayers));
+    const { resetForNextTurn } = result.current;
+
+    act(() => {
+      resetForNextTurn();
+    });
+
+    const { players } = result.current;
+    players.forEach(player => {
+      expect(player.hand).toEqual([]);
+      expect(player.isPicker).toBe(false);
+      expect(player.cardToPlay).toEqual({});
+    });
+  });
+
+  it('should reset for the next round', () => {
+    const { result } = renderHook(() => usePlayer(initialPlayers));
+    const { resetForNextRound } = result.current;
+
+    act(() => {
+      resetForNextRound();
+    });
+
+    const { players } = result.current;
+    players.forEach(player => {
+      expect(player.hand).toEqual([]);
+      expect(player.isPicker).toBe(false);
+      expect(player.cardToPlay).toEqual({});
+      expect(player.wonCards).toEqual([]);
+      expect(player.wonCardsTotal).toBe(0);
+    });
+  });
+});
--- src/Components/Hooks/usePlayer.tsx
+++ src/Components/Hooks/usePlayer.tsx
@@ -1,51 +1,140 @@
-import react, { useState, useEffect, useReducer } from 'react';
-import { player as playerObj } from '../../GameLogic/players';
-import { GamePlayer } from '../../types';
+import { useReducer } from 'react';
+import { cardScoreValues } from '../../GameLogic/deck';
+import { TableCard, Player } from '../../types';
 
-type PlayerReducerState = {
-  id?: string;
-  hand?: string[];
-};
+type PlayerAction =
+  | { type: 'SET_HAND'; playerId: string; hand: string[] }
+  | { type: 'MAKE_PICKER'; playerId: string; cards: string[] }
+  | { type: 'ADD_WON_CARDS'; playerId: string; cards: string[] }
+  | { type: 'PLAY_CARD'; playerId: string; card: string }
+  | { type: 'TOTAL_WON_CARDS' }
+  | { type: 'RESET_FOR_NEXT_TURN' }
+  | { type: 'RESET_FOR_NEXT_ROUND' }
+  | { type: 'RESET_FOR_NEW_GAME' };
 
-type ACTIONTYPE = { type: 'updateId' } | { type: 'updateHand'; hand: string[] };
+const playerReducer = (state: Player[], action: PlayerAction): Player[] => {
+  switch (action.type) {
+    case 'SET_HAND':
+      return state.map(player =>
+        player.id === action.playerId
+          ? { ...player, hand: action.hand }
+          : player
+      );
+    case 'MAKE_PICKER':
+      return state.map(player =>
+        player.id === action.playerId
+          ? {
+              ...player,
+              isPicker: true,
+              wonCards: [...player.wonCards, ...action.cards]
+            }
+          : player
+      );
+    case 'ADD_WON_CARDS':
+      return state.map(player =>
+        player.id === action.playerId
+          ? { ...player, wonCards: [...player.wonCards, ...action.cards] }
+          : player
+      );
+    case 'TOTAL_WON_CARDS':
+      return state.map((player, index) => {
+        let total = 0;
+        console.log('player', index, ' ', player.wonCardsTotal);
+        player.wonCards.forEach((c: string) => {
+          const split = c.split('');
+          const cardType = split[0];
+          const suit = split[1]; // this might not matter
+          total += cardScoreValues[cardType];
+        });
+        console.log('player', index, ' ', total);
 
-function playerReducer(
-  state: PlayerReducerState,
-  action: ACTIONTYPE
-): PlayerReducerState {
-  if (action.type === 'updateId') {
-    return {
-      id: '123445'
-    };
-  } else if (action.type === 'updateHand') {
-    return {
-      hand: [...action.hand]
-    };
+        return { ...player, wonCardsTotal: total };
+      });
+    case 'PLAY_CARD':
+      return state.map(player =>
+        player.id === action.playerId
+          ? {
+              ...player,
+              hand: player.hand.filter(card => card !== action.card),
+              cardToPlay: {
+                player: player.id,
+                card: action.card
+              }
+            }
+          : player
+      );
+    case 'RESET_FOR_NEXT_TURN':
+      return state.map(player => ({
+        ...player,
+        hand: [],
+        isPicker: false,
+        cardToPlay: {} as TableCard
+      }));
+    case 'RESET_FOR_NEXT_ROUND':
+      return state.map(player => ({
+        ...player,
+        hand: [],
+        isPicker: false,
+        cardToPlay: {} as TableCard,
+        wonCards: [],
+        wonCardsTotal: 0
+      }));
+    case 'RESET_FOR_NEW_GAME':
+      return state.map(player => ({
+        ...player,
+        hand: [] as string[],
+        isPicker: false,
+        cardToPlay: {} as TableCard,
+        wonCards: [] as string[],
+        wonCardsTotal: 0,
+        score: 0
+      }));
+    default:
+      return state;
   }
-  throw Error('Unknown action.');
-}
-
-const initialState = { id: 'player0', hand: [] };
-
-type PlayerInit = {
-  id?: string;
-  hand?: string[];
 };
 
-type Return = {
-  state: PlayerReducerState;
-  dispatch: react.Dispatch<ACTIONTYPE>;
-};
+const usePlayer = (initialPlayers: Player[]) => {
+  const [players, dispatch] = useReducer(playerReducer, initialPlayers);
 
-// export const playerFactory
+  const setHand = (playerId: string, hand: string[]) => {
+    dispatch({ type: 'SET_HAND', playerId, hand });
+  };
 
-export const usePlayer = (player: PlayerInit): Return => {
-  const [state, dispatch] = useReducer(playerReducer, initialState);
+  const makePicker = (playerId: string, cards: string[]) => {
+    dispatch({ type: 'MAKE_PICKER', playerId, cards });
+  };
 
-  useEffect(() => {}, []);
+  const addWonCard = (playerId: string, cards: string[]) => {
+    dispatch({ type: 'ADD_WON_CARDS', playerId, cards });
+  };
+
+  const playCard = (playerId: string, card: string) => {
+    dispatch({ type: 'PLAY_CARD', playerId, card });
+  };
+
+  const totalWonCards = () => {
+    dispatch({ type: 'TOTAL_WON_CARDS' });
+  };
+
+  const resetForNextTurn = () => {
+    dispatch({ type: 'RESET_FOR_NEXT_TURN' });
+  };
+
+  const resetForNextRound = () => {
+    dispatch({ type: 'RESET_FOR_NEXT_ROUND' });
+  };
 
   return {
-    state,
-    dispatch
+    players,
+    setHand,
+    makePicker,
+    addWonCard,
+    totalWonCards,
+    playCard,
+    resetForNextTurn,
+    resetForNextRound
   };
 };
+
+export default usePlayer;
