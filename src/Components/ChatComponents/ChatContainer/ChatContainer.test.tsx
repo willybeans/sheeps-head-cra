f3bb845e6d27f47815283ad9059411684c19e5e5
Will Wedmedyk <willwedmedyk@gmail.com>
2023-08-30 19:32:10 -0400

    ww/initial chatcontainer component test file

---
+++ src/Components/ChatComponents/ChatContainer/ChatContainer.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import ChatContainer from './ChatContainer';
+
+describe('ChatContainer', () => {
+  it('placeholder', () => {
+    expect(1).toEqual(1);
+  });
+});
