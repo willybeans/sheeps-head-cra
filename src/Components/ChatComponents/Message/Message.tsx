a4d8f8a3d2444f65f0e5f50c519cbe76266b5b05
Will Wedmedyk <willwedmedyk@gmail.com>
2023-09-06 19:32:10 -0400

    ww/update messages display and props

--- src/Components/ChatComponents/Message/Message.tsx
+++ src/Components/ChatComponents/Message/Message.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './message.module.scss';
-import { CardProps } from '../../../types';
+import { MessageBody } from '../../../types';
 
-const Message: React.FC<CardProps> = ({ card, cardClick }) => {
+const Message: React.FC<MessageBody> = ({ name, content, time }) => {
   return (
-    <div data-testid={'message-container-test'} className={`${styles.message}`}>
-      <div>Name</div>
-      <div>Message</div>
+    <div data-testid={'message-test'} className={`${styles.message}`}>
+      <div className={styles.name}>{name}:</div>
+      <div className={styles.time}>{time}:</div>
+      <div className={styles.content}>{content}</div>
     </div>
   );
 };
