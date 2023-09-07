4bf17be2e0c9ecf387a74dbdb9e9c5827bffccc0
Will Wedmedyk <willwedmedyk@gmail.com>
2023-09-07 19:32:10 -0400

    ww/update message container component

--- src/Components/ChatComponents/MessageContainer/MessageContainer.tsx
+++ src/Components/ChatComponents/MessageContainer/MessageContainer.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import styles from './messageContainer.module.scss';
-import { CardProps } from '../../../types';
 import Message from '../Message/Message';
+import useWebSocket from '../../Hooks/useWebSocket';
 
-const MessageContainer: React.FC<CardProps> = ({ card, cardClick }) => {
+const MessageContainer: React.FC = () => {
+  const { receivedMessages } = useWebSocket();
   return (
     <div
       data-testid={'message-container-test'}
       className={`${styles.messageContainer}`}
-    ></div>
+    >
+      message container
+      {receivedMessages.map((obj, i) => {
+        return (
+          <Message
+            key={`message-${i}`}
+            time={obj.time}
+            name={obj.name}
+            content={obj.content}
+          />
+        );
+      })}
+    </div>
   );
 };
 
