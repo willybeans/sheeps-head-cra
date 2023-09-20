3df938b58c36beebbfdb2a436ba6926bb6129e6e
Will Wedmedyk <willwedmedyk@gmail.com>
2023-09-20 19:32:10 -0400

    ww/update message component

--- src/Components/ChatComponents/Message/Message.tsx
+++ src/Components/ChatComponents/Message/Message.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import styles from './message.module.scss';
 import { MessageBody } from '../../../types';
+import { Flex, Text, chakra } from '@chakra-ui/react';
 
 const Message: React.FC<MessageBody> = ({ name, content, time }) => {
   return (
-    <div data-testid={'message-test'} className={`${styles.message}`}>
-      <div className={styles.name}>{name}:</div>
-      <div className={styles.time}>{time}:</div>
-      <div className={styles.content}>{content}</div>
-    </div>
+    <Flex
+      direction={'row'}
+      data-testid={'message-test'}
+      justify={'flex-start'}
+      width="100%"
+    >
+      <Text align="left">
+        <chakra.span fontWeight="extrabold">[{time}]</chakra.span>
+        <chakra.span fontWeight="extrabold">{name}: </chakra.span>
+        {content}
+      </Text>
+    </Flex>
   );
 };
 
