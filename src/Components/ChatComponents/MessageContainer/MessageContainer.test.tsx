fae3382f373ba62432c803360075ae951d69c2d0
Will Wedmedyk <willwedmedyk@gmail.com>
2023-09-25 19:32:10 -0400

    ww/messagecontainer tests

--- src/Components/ChatComponents/MessageContainer/MessageContainer.test.tsx
+++ src/Components/ChatComponents/MessageContainer/MessageContainer.test.tsx
@@ -1,9 +1,37 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import MessageContainer from './MessageContainer';
+import receivedMessages from '../../Hooks/useWebSocket';
+
+jest.mock('../../Hooks/useWebSocket', () => ({
+  __esModule: true,
+  default: jest.fn()
+}));
 
 describe('MessageContainer', () => {
-  it('placeholder', () => {
-    expect(1).toEqual(1);
+  const mockUseWebSocket = receivedMessages as jest.Mock;
+
+  it('renders the message container component', () => {
+    mockUseWebSocket.mockReturnValue({
+      receivedMessages: [
+        { name: 'John', content: 'Hello', time: '1632767888000' }
+        // Add more mock messages as needed
+      ]
+    });
+
+    const { getByTestId } = render(<MessageContainer />);
+
+    const messageContainer = getByTestId('message-container-test');
+    expect(messageContainer).toBeInTheDocument();
+  });
+
+  it('renders messages correctly', () => {
+    mockUseWebSocket.mockReturnValue({
+      receivedMessages: [{ name: 'John', content: 'Hello', time: '12:34' }]
+    });
+
+    const { getByText } = render(<MessageContainer />);
+
+    const messageContent = getByText('Hello');
+    expect(messageContent).toBeInTheDocument();
   });
 });
