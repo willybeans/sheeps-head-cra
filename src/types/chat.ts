e9cefd554179a383b371e23f6b645f137e625e5c
Will Wedmedyk <willwedmedyk@gmail.com>
2023-10-06 19:32:10 -0400

    ww/update types for chat websocket connection

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -6,8 +6,23 @@ export interface Messages {
   [key: string]: string;
 }
 
+export type ApiMessage = {
+  chat_room_id: string;
+  user_id: string;
+  content: string;
+  id: string;
+  sent_at: string;
+  contentType: 'chat' | 'game';
+};
+
 export interface MessageBody {
   time: string;
   name: string;
   content: string;
 }
+
+export type ChatFeed = MessageBody[];
+
+export type WebSocketSend = (
+  data: string | ArrayBufferLike | Blob | ArrayBufferView
+) => void;
